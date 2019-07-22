02f209c5487dfa98e0bf66fd7c1554fd656e3664
Tushar <tusharguptat635@gmail.com>
2019-07-23 02:15:24 +0530

    fatal mistake averted

--- express2.js
+++ express2.js
@@ -1,37 +1,18 @@
-const express=require('express');
+const express = require('express');
 const http = require('http');
 const bodyparser = require('body-parser');
 //used for post req puts the json data in req body
 const dishRouter = require('./routes/dishrouter');
 const app = express();
 const morgan = require('morgan');
-const hostname='local host';
+const hostname = 'local host';
 const port = 3000;
 app.use(morgan('dev'));
 app.use(bodyparser.json());
-app.use('/dishes',dishRouter);
-app.get('/dishes/:dishid', function (req, res, next) {
-    res.end("We will be sending details of the dish "+req.params.dishid+' to you');
-});
-app.post('/dishes/:dishid', function (req, res, next) {
-    res.statusCode = 403;
-    res.end('put ooperation not supported on dishes '+req.params.dishid);
-
-});
-app.put('/dishes/:dishid',function(req,res,next)
-{
-    res.write('Updating the dish '+req.params.dishid);
-
-    res.end('Will update the dish '+req.body['name']+' with details '+req.body['description']);
-});
-app.delete('/dishes/:dishid',function(req,res,next)
-{
-    res.end('Deleting the dish'+req.params.dishid);
-});
-app.use(function(req,res,next)
-{
-    res.statusCode=200;
-    res.setHeader('Content-type','text/html');
+app.use('/dishes', dishRouter);
+app.use(function (req, res, next) {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
     res.end("This is an express enabled server");
 });
 app.listen(port);
--- routes/dishrouter.js
+++ routes/dishrouter.js
@@ -1,26 +1,45 @@
-const express= require('express');
+const express = require('express');
 const bodyparser = require('body-parser');
 
 const dishRouter = express.Router();
 dishRouter.use(bodyparser.json());
 dishRouter.route('/')
-.all(function (req, res, next) {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text');
-    next();
-    //this will continue to search for additional edit on /dishes
-    //the same res and req objects will be passed to any other get put or post request
-}).get(function (req, res, next) {
-    res.end("We will be sending you all the dishes");
+    .all(function (req, res, next) {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text');
+        next();
+        //this will continue to search for additional edit on /dishes
+        //the same res and req objects will be passed to any other get put or post request
+    }).get(function (req, res, next) {
+        res.end("We will be sending you all the dishes");
+    }).post(function (req, res, next) {
+        res.end('wll add the dish' + req.body.name + ' with description ' + req.body.description);
+        //this will be done by using the body parser
+
+    }).delete(function (req, res, next) {
+        res.end('Deleting all dishes');
+    }).put(function (req, res, next) {
+
+        res.statusCode = 403;
+        res.end('This operation is not allowed');
+    });
+dishRouter.route('/:dishid')
+    .all(function (req, res, next) {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text');
+        next();
+        //this will continue to search for additional edit on /dishes
+        //the same res and req objects will be passed to any other get put or post request
+    }).get( function (req, res, next) {
+    res.end("We will be sending details of the dish " + req.params.dishid + ' to you');
 }).post(function (req, res, next) {
-    res.end('wll add the dish' + req.body.name + ' with description ' + req.body.description);
-    //this will be done by using the body parser
-
-}).delete(function (req, res, next) {
-    res.end('Deleting all dishes');
-}).put(function (req, res, next) {
-
     res.statusCode = 403;
-    res.end('This operation is not allowed');
+    res.end('post ooperation not supported on dishes ' + req.params.dishid);
+}).put(function (req, res, next) {
+    res.write('Updating the dish ' + req.params.dishid);
+
+    res.end('Will update the dish ' + req.body['name'] + ' with details ' + req.body['description']);
+}).delete(function (req, res, next) {
+    res.end('Deleting the dish' + req.params.dishid);
 });
-module.exports=dishRouter();
+module.exports = dishRouter;
